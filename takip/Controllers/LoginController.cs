6769cc31982f38821c9ab69d5b235651750a0e83
bedirhankuru <kurubedirhansabri@gmail.com>
2025-06-08 00:59:50 +0300

    Add project files.

---
+++ takip/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using takip.Models;
+namespace takip.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        IsTakipDBEntities entity = new IsTakipDBEntities(); 
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
